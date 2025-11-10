981198c395eb712f07b63bec989324e10c8c909b
Miguel Gargurevich <miguel.gargurevich@gmail.com>
2025-11-09 22:31:40 -0500

    feat: Add reCAPTCHA validation service in backend

--- backend/PCM.Infrastructure/Services/ReCaptchaService.cs
+++ backend/PCM.Infrastructure/Services/ReCaptchaService.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using PCM.Application.Interfaces;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace PCM.Infrastructure.Services;
 
@@ -98,16 +99,22 @@ public class ReCaptchaService : IReCaptchaService
 
     private class ReCaptchaResponse
     {
+        [JsonPropertyName("success")]
         public bool Success { get; set; }
         
+        [JsonPropertyName("score")]
         public double? Score { get; set; }
         
+        [JsonPropertyName("action")]
         public string? Action { get; set; }
         
+        [JsonPropertyName("challenge_ts")]
         public DateTime? ChallengeTs { get; set; }
         
+        [JsonPropertyName("hostname")]
         public string? Hostname { get; set; }
         
+        [JsonPropertyName("error-codes")]
         public string[]? ErrorCodes { get; set; }
     }
 }
